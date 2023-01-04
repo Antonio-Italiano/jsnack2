b8ccb1e902be7b8fd2d7b353b74fea7f47a812f9
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-04 14:08:05 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* 
+A partire da un array di stringhe, crea un secondo array formattando le
+stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] 
+*/
+
+
+
+// ARRAY INIZIALE
+const listBrands = ['samsung', 'INTEL', 'euRotech', 'optomA'];
+
+// ARRAY CON LE PAROLE FORMATTATE
+const newList = [];
+
+// CREO UN CICLO CHE GIRA DENTRO L'ARRAY INIZIALE
+listBrands.forEach(brand => {
+})
+
+// STAMPO IN CONSOLE IL CONTENUTO DELLA NEW LIST IN CONSOLE
+console.log(newList);
